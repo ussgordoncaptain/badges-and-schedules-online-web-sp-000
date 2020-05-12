ee63da9a736c6e3a414a95d797543b8262f0bb2e
Edmund Nelson <edmund.nelson.b@gmail.com>
2020-05-12 12:51:49 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end 
+def batch_badge_creator(arr_of_names)
+  final= []
+  arr_of_names.each do |name|
+    string = "Hello, my name is #{name}."
+    final << string 
+  end
+  return final
+end
+
+def assign_rooms(arr_of_names)
+  final=[]
+  arr_of_names.each_with_index do |name,index|
+    string_to_insert= "Hello, #{name}! You'll be assigned to room #{index+1}!"
+    final << string_to_insert
+  end 
+  return final 
+end 
+
+def printer(arr_of_names)
+  badges_arr=batch_badge_creator(arr_of_names)
+  rooms_arr = assign_rooms(arr_of_names)
+  names_count = arr_of_names.length
+  ii=0 
+  while (ii<names_count)
+  puts badges_arr[ii]
+  puts rooms_arr[ii]
+  ii +=1
+end
+end 
